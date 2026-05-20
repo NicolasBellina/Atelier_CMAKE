9ed1d09eb0e83f16660cda82cbd518229fade011
Nicolas Bellina <nicolasbellina@MacBook-Pro-de-Nicolas.local>
2026-05-20 09:48:19 +0200

    Remove build/ from git tracking and add to .gitignore

--- build/src/lib/CMakeFiles/lib.dir/calculator.c.o.d
+++
@@ -1,3 +0,0 @@
-src/lib/CMakeFiles/lib.dir/calculator.c.o: \
-  /Users/nicolasbellina/Documents/ESGI\ M1/Atelier_CMAKE/src/lib/calculator.c \
-  /Users/nicolasbellina/Documents/ESGI\ M1/Atelier_CMAKE/src/lib/./../../include/calculator.h
